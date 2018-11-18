c045501d96b0c90a6461cf412cbb841f31dcfc76
Kevin Ma <kbma.kevin@gmail.com>
2018-11-17 19:27:52 -0500

    Added demo of how to use Jinja2 templates with flask application

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
-from flask import Flask
-import sklearn.datasets
+from flask import (
+    Flask,
+    render_template,
+)
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+
+# run code from another python file
+from iris import (data_set, main)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
-# run code from another python file
-import iris
+
+@app.route('/user/<username>')
+def index(username):
+    return render_template('index.html', username=username)
+
+
+@app.route('/iris')
+def iris():
+    return render_template('iris.html',
+                           feature_name_colspan=len(data_set.feature_names),
+                           data_set=data_set)
+
 
 if __name__ == '__main__':
     app.run()
--- iris.py
+++ iris.py
@@ -2,26 +2,25 @@ import numpy as np
 from sklearn.datasets import load_iris
 from sklearn import tree
 
+# collect training data
+data_set = load_iris()
+test_idx = [0, 50, 100]
 
 def main():
-    # collect training data
-    iris = load_iris()
-    test_idx = [0, 50, 100]
-
-    print('iris.feature_names={}'.format(iris.feature_names))
-    print('iris.target_names={}'.format(iris.target_names))
-    print('iris sample data={}'.format(iris.data[0]))
+    print('iris.feature_names={}'.format(data_set.feature_names))
+    print('iris.target_names={}'.format(data_set.target_names))
+    print('iris sample data={}'.format(data_set.data[0]))
     # print (iris.target[0])
     # for i in range(len(iris.target)):
     # 	print ("Example {}: label {} features {}".format(i, iris.target[i], iris.data[i]))
 
     # training data
-    train_target = np.delete(iris.target, test_idx)
-    train_data = np.delete(iris.data, test_idx, axis=0)
+    train_target = np.delete(data_set.target, test_idx)
+    train_data = np.delete(data_set.data, test_idx, axis=0)
 
     # testing data
-    test_target = iris.target[test_idx]
-    test_data = iris.data[test_idx]
+    test_target = data_set.target[test_idx]
+    test_data = data_set.data[test_idx]
 
     # training the classifier
     clf = tree.DecisionTreeClassifier()
@@ -34,13 +33,9 @@ def main():
     # visualizing the tree
     import pydotplus
     dot_data = tree.export_graphviz(clf, out_file=None,
-                                    feature_names=iris.feature_names,
-                                    class_names=iris.target_names,
+                                    feature_names=data_set.feature_names,
+                                    class_names=data_set.target_names,
                                     filled=True, rounded=True,
                                     special_characters=True)
     graph = pydotplus.graph_from_dot_data(dot_data)
     graph.write_pdf("./iris.pdf")
-
-
-# execute main function - entry point
-main()
